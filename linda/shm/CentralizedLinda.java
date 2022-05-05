d909ce12bbb4adbe15c6f1536f0cf5e86618dd3e
Akodix <nathan.chavas@gmail.com>
2022-05-05 14:33:24 +0200

    Import des méthodes à override

--- linda/shm/CentralizedLinda.java
+++ linda/shm/CentralizedLinda.java
@@ -4,12 +4,56 @@ import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
 
+import java.util.Collection;
+
 /** Shared memory implementation of Linda. */
 public class CentralizedLinda implements Linda {
 	
     public CentralizedLinda() {
     }
 
-    // TO BE COMPLETED
+    @Override
+    public void write(Tuple t) {
 
+    }
+
+    @Override
+    public Tuple take(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Tuple read(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Tuple tryTake(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Tuple tryRead(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Collection<Tuple> takeAll(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public Collection<Tuple> readAll(Tuple template) {
+        return null;
+    }
+
+    @Override
+    public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {
+
+    }
+
+    @Override
+    public void debug(String prefix) {
+
+    }
 }
